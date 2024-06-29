d8c2bf8b3bdb2858877a2d1dc5ddcb95961d6179
Edward Lim <ed.lim199@gmail.com>
2024-06-29 19:33:18 +1000

    i think ive got the tab bar working with routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthContext } from './AuthContext';
-import AppRouter from './AppRouter';
-
+import Journal from './Journal';
+import Friends from './Friends';
+import Profile from './Profile';
+import Login from './Login';
+import SignUp from './SignUp';
+import LoadingScreen from './LoadingScreen';
+import Navbar from './Navbar';
 
 function App() {
+  const aC = useContext(AuthContext)
+
   return (
-  <AuthContext.Provider>
-    <AppRouter/>
-  </AuthContext.Provider>
+    <BrowserRouter>
+      {aC?.isLoggedIn && <Navbar/>}
+      <Routes>
+        <Route exact path="" element={<LoadingScreen />} />
+        <Route exact path="/journal" element={<Journal />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<SignUp/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
