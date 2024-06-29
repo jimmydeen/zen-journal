fbddb835714dd6ff0ca6ec4bd995ad33b0e499b9
Joong Do Chiang <jchi0099@student.monash.edu>
2024-06-30 00:09:22 +1000

    Fix Supabase missing URL error (REACT_APP_ prefix in .env)

--- frontend/src/Supabase.js
+++ frontend/src/Supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase_url = process.env.SUPABASE_URL;
-const supabase_key = process.env.SUPABASE_KEY;
+
+const supabase_url = process.env.REACT_APP_SUPABASE_URL;
+const supabase_key = process.env.REACT_APP_SUPABASE_KEY;
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(supabase_url, supabase_key);
