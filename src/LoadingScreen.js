c823ce51e472500b4d34e270da840048e8583405
Edward Lim <ed.lim199@gmail.com>
2024-06-29 14:42:30 +1000

    profile close to done

---
+++ src/LoadingScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoadingScreen.css';
+
+function LoadingScreen() {
+  return (
+    <div className="loading-screen">
+      <h1 className="loading-text">write your story</h1>
+    </div>
+  );
+}
+
+export default LoadingScreen;
