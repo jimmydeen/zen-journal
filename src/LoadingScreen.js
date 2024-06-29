a782b2fe8283f080f574244690f7ebca5a4afdb0
Joong Do Chiang <jchi0099@student.monash.edu>
2024-06-29 14:55:19 +1000

    Add landing page animation

--- src/LoadingScreen.js
+++ src/LoadingScreen.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './LoadingScreen.css';
 
 function LoadingScreen() {
+  const text = "Write your story";
+
   return (
     <div className="loading-screen">
-      <h1 className="loading-text">write your story</h1>
+      <h1 className="loading-text">
+        {text.split("").map((char, index) => (
+          <span key={index} style={{ animationDelay: `${index * 0.1}s` }}>
+            {char === " " ? "\u00A0" : char}
+          </span>
+        ))}
+      </h1>
     </div>
   );
 }
