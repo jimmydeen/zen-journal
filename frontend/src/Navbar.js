5f5292eeff49152f44406fcf8cabcad6b9111ef0
Edward Lim <ed.lim199@gmail.com>
2024-06-30 19:30:38 +1000

    log out button is in the nav bar

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,10 +1,22 @@
-import React from "react"
+import React, { useContext } from "react"
+import { AuthContext } from "./AuthContext"
 import { Link } from 'react-router-dom'
+import { supabase } from "./Supabase"
 
 function Navbar() {
+  const {ac, setAc} = useContext(AuthContext)
+  function logOutHandler() {
+    let { error } = supabase.auth.signOut()
+    if (!error) {
+      setAc({isLoggedIn: false})
+    }
+    console.log(error)
+    alert('Youre logged out now.')
+  }
   return (
       <nav className="tab-bar">
-        <Link to="/" className="tab">Journal</Link>
+        <button className="logout-button" onClick={logOutHandler}>Log Out</button>
+        <Link to="/journal" className="tab">Journal</Link>
         <Link to="/friends" className="tab">Friends</Link>
         <Link to="/profile" className="tab">Profile</Link>
       </nav>
