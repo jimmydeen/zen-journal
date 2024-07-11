efa3b0f42c80d06e738a35a42c8f581d85feffb3
Edward Lim <ed.lim199@gmail.com>
2024-07-11 22:42:53 +1000

    no longer using context, custom hook in useLoggedInStatus

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,16 +1,13 @@
-import React, { useContext } from "react"
-import { AuthContext } from "./AuthContext"
+import React from "react"
 import { Link, useNavigate } from 'react-router-dom'
 import { supabase } from "./Supabase"
 
 function Navbar() {
-  const {ac, setAc} = useContext(AuthContext)
   const navigate = useNavigate()
   function logOutHandler() {
     let { error } = supabase.auth.signOut()
     if (!error) {
-      setAc({isLoggedIn: false})
-      navigate('')
+      navigate('/')
     } else {
       console.log(error)
     }
@@ -18,9 +15,9 @@ function Navbar() {
   return (
       <nav className="tab-bar">
         <button className="logout-button" onClick={logOutHandler}>Log Out</button>
-        <Link to="/journal" className="tab">Journal</Link>
-        <Link to="/friends" className="tab">Friends</Link>
-        <Link to="/profile" className="tab">Profile</Link>
+        <Link to="/user/journal" className="tab">Journal</Link>
+        <Link to="/user/friends" className="tab">Friends</Link>
+        <Link to="/user/profile" className="tab">Profile</Link>
       </nav>
   )
 }
---
+++ frontend/src/useLoggedInStatus.js
@@ -0,0 +1,30 @@
+import { supabase } from "./Supabase";
+import { useEffect, useState } from "react";
+
+export function useIsLoggedInStatus() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  useEffect(() => {
+    let ignore = false
+    async function getLoggedInStatus() {
+      const {data, error} = await supabase.auth.getSession()
+
+      if (error) throw error
+    
+      if (!ignore) {
+        if (data.session !== null) {
+          console.log('they are logged in, now setting true')
+          setIsLoggedIn(true)
+        } else {
+          console.log('they are not logged in, now setting false')
+          setIsLoggedIn(false)
+        }
+      }
+    }
+    getLoggedInStatus()
+    return () => {ignore = true}
+  }, [])
+
+  return isLoggedIn
+}
+
+export default useIsLoggedInStatus
