c1c04723e42c22ef3e3f849a0baba4ac000f9b77
Edward Lim <ed.lim199@gmail.com>
2024-08-26 16:59:42 +1000

    nitpick extraneous line from Login.test.js

--- src/pages/auth/Login.test.js
+++ src/pages/auth/Login.test.js
@@ -1,7 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom'
 import Login from './Login';
-import { userInfo } from 'os';
 
 /*
  don't like the current use of router to simply nest the component we're
