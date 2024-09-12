36485d9c247b38e196a8a0f93038d2d18d718e3b
Edward Lim <ed.lim199@gmail.com>
2024-09-12 11:18:43 +1000

    added environment variables to environment named production and also changed router's basename

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import LoadingScreen from './pages/LoadingScreen';
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={`/${process.env.REACT_APP_PUBLIC_URL}`}>
       <Routes>
         <Route exact path="" element={<LoadingScreen />} />
         <Route path="/user" element={<UserPage/>}>
